a8f372d9c3d07ff10b9fa3a23e21d5a9eaf8269f
martinmsaavedra <martin@bildung.uy>
2021-01-30 22:01:06 +0000

    task 4

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -14,24 +14,26 @@ void shell_sort(int *array, size_t size)
 	int number;
 
 	if (array == NULL || size < 2)
-			return;
+		return;
 
-    while (h < size / 3) {
-        h = 3 * h + 1;
-    }
+	while (h < size / 3)
+	{
+		h = 3 * h + 1;
+	}
 
-    while (h > 0) {
-        for (i = h; i < size; i += h) {
-            for (n = i; n > 0 && array[n] < array[n-h]; n -= h) 
+	while (h > 0)
+	{
+		for (i = h; i < size; i += h)
+		{
+			for (n = i; n > 0 && array[n] < array[n - h]; n -= h)
 			{
-                number = array[n];
-                array[n] = array[n-h];
-                array[n-h] = number;
-				
-            }
-        }
-        h = h - 1;
+				number = array[n];
+				array[n] = array[n - h];
+				array[n - h] = number;
+			}
+		}
+		h = h - 1;
 		h = h / 3;
 		print_array(array, size);
-    }
+	}
 }
