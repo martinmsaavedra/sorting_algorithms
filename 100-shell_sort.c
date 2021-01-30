ba12e18491b5f59e3ea79840aca7de10cef47b1c
martinmsaavedra <martin@bildung.uy>
2021-01-30 22:10:10 +0000

    task 4

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -2,7 +2,7 @@
 
 /**
 *shell_sort - sorts an array of integers in ascending order
-using the Shell sort algorithm, using the Knuth sequence
+*using the Shell sort algorithm, using the Knuth sequence
 *@array: the array
 *@size: the size
 *Return: Nothing
@@ -18,12 +18,12 @@ void shell_sort(int *array, size_t size)
 
 	while (h < size / 3)
 	{
-		h = 3 * h + 1;
+		h = h * 3 + 1;
 	}
 
 	while (h > 0)
 	{
-		for (i = h; i < size; i += h)
+		for (i = h; i < size; i++)
 		{
 			for (n = i; n > 0 && array[n] < array[n - h]; n -= h)
 			{
