64193a0501a302ec4e44f579a453486e0e6063b2
martinmsaavedra <martin@bildung.uy>
2021-02-01 18:23:56 +0000

    task 1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -15,18 +15,18 @@ void insertion_sort_list(listint_t **list)
 		return;
 
 	aux = *list;
+
 	while (aux)
 	{
 		if (aux->prev)
 		{
 			if (aux->prev->n > aux->n)
 			{
-				swap_nodes(list, aux->prev, aux);
-				print_list(*list);
 				temp = aux;
 				temp2 = aux;
+				swap_nodes(list, aux->prev, aux);
 				aux = aux->next;
-
+				print_list(*list);
 				while (temp2)
 				{
 					if (temp2->n > temp->n)
@@ -52,12 +52,13 @@ void insertion_sort_list(listint_t **list)
 
 void swap_nodes(listint_t **list, listint_t *node_A, listint_t *node_B)
 {
-	node_A->prev = node_B;
-	node_B->next = node_A;
+	
 	if (node_A->prev == NULL && node_B->next == NULL)
 	{
 		node_A->next = NULL;
 		node_B->prev = NULL;
+		node_A->prev = node_B;
+		node_B->next = node_A;
 		*list = node_B;
 		return;
 	}
@@ -66,6 +67,8 @@ void swap_nodes(listint_t **list, listint_t *node_A, listint_t *node_B)
 		listint_t *t1 = node_B->next;
 
 		node_A->next = t1;
+		node_A->prev = node_B;
+		node_B->next = node_A;
 		node_B->prev = NULL;
 		t1->prev = node_A;
 		*list = node_B;
@@ -78,6 +81,8 @@ void swap_nodes(listint_t **list, listint_t *node_A, listint_t *node_B)
 
 		node_B->prev = t1;
 		t1->next = node_B;
+		node_B->next = node_A;
+		node_A->prev = node_B;
 		node_A->next = NULL;
 		return;
 	}
@@ -87,29 +92,11 @@ void swap_nodes(listint_t **list, listint_t *node_A, listint_t *node_B)
 		listint_t *t2 = node_B->next;
 
 		node_A->next = t2;
+		node_A->prev = node_B;
+		node_B->next = node_A;
 		node_B->prev = t1;
 		t1->next = node_B;
 		t2->prev = node_A;
 		return;
 	}
 }
-
-/**
- * swap_nodes2 - swap the nodes of the list
- * @list: Double linked list
- * @node_A: Node A
- * @node_B: Node B
- * Return: Nothing
- 
-
-void swap_nodes2(listint_t **list, listint_t *node_A, listint_t *node_B)
-{
-	listint_t *t1 = node_A->prev;
-	listint_t *t2 = node_B->next;
-
-	node_A->next = t2;
-	node_B->prev = t1;
-	t1->next = node_B;
-	t2->prev = node_A;
-}
-*/
