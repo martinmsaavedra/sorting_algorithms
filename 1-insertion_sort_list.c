fb87ada0231abd9268497bdce98c87140a652675
martinmsaavedra <martin@bildung.uy>
2021-02-01 18:10:55 +0000

    fix 1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -83,13 +83,25 @@ void swap_nodes(listint_t **list, listint_t *node_A, listint_t *node_B)
 	}
 	else
 	{
-		listint_t *t1 = node_A->prev;
-		listint_t *t2 = node_B->next;
-
-		node_A->next = t2;
-		node_B->prev = t1;
-		t1->next = node_B;
-		t2->prev = node_A;
-		return;
+		swap_nodes2(list, node_A, node_B);
 	}
+}
+
+/**
+ * swap_nodes2 - swap the nodes of the list
+ * @list: Double linked list
+ * @node_A: Node A
+ * @node_B: Node B
+ * Return: Nothing
+ */
+
+void swap_nodes2(listint_t **list, listint_t *node_A, listint_t *node_B)
+{
+	listint_t *t1 = node_A->prev;
+	listint_t *t2 = node_B->next;
+
+	node_A->next = t2;
+	node_B->prev = t1;
+	t1->next = node_B;
+	t2->prev = node_A;
 }
