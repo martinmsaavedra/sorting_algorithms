7911a75e3a2e5addf4af8f4174b3ca3d934e5e74
martinmsaavedra <martin@bildung.uy>
2021-02-01 17:34:57 +0000

    fix 1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -15,18 +15,18 @@ void insertion_sort_list(listint_t **list)
 		return;
 
 	aux = *list;
-
 	while (aux)
 	{
 		if (aux->prev)
 		{
 			if (aux->prev->n > aux->n)
 			{
+				swap_nodes(list, aux->prev, aux);
+				print_list(*list);
 				temp = aux;
 				temp2 = aux;
-				swap_nodes(list, aux->prev, aux);
 				aux = aux->next;
-				print_list(*list);
+
 				while (temp2)
 				{
 					if (temp2->n > temp->n)
@@ -52,13 +52,20 @@ void insertion_sort_list(listint_t **list)
 
 void swap_nodes(listint_t **list, listint_t *node_A, listint_t *node_B)
 {
+	node_A->prev = node_B;
+	node_B->next = node_A;
+	if (node_A->prev == NULL && node_B->next == NULL)
+	{
+		node_A->next = NULL;
+		node_B->prev = NULL;
+		*list = node_B;
+		return;
+	}
 	if (node_A->prev == NULL)
 	{
 		listint_t *t1 = node_B->next;
 
 		node_A->next = t1;
-		node_A->prev = node_B;
-		node_B->next = node_A;
 		node_B->prev = NULL;
 		t1->prev = node_A;
 		*list = node_B;
@@ -71,8 +78,6 @@ void swap_nodes(listint_t **list, listint_t *node_A, listint_t *node_B)
 
 		node_B->prev = t1;
 		t1->next = node_B;
-		node_B->next = node_A;
-		node_A->prev = node_B;
 		node_A->next = NULL;
 		return;
 	}
@@ -82,8 +87,6 @@ void swap_nodes(listint_t **list, listint_t *node_A, listint_t *node_B)
 		listint_t *t2 = node_B->next;
 
 		node_A->next = t2;
-		node_A->prev = node_B;
-		node_B->next = node_A;
 		node_B->prev = t1;
 		t1->next = node_B;
 		t2->prev = node_A;
