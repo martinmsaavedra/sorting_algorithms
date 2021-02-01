930dc01306d62c19e1acc772fb39b609c245a5ba
martinmsaavedra <martin@bildung.uy>
2021-02-01 19:40:20 +0000

    sort.h

--- sort.h
+++ sort.h
@@ -44,4 +44,6 @@ void heap_sort(int *array, size_t size);
 void heapify(int *array, size_t size, size_t base, size_t root);
 void swap_nodes2(listint_t **list, listint_t *node_A, listint_t *node_B);
 void cocktail_sort_list(listint_t **list);
+void quick_sort_hoare(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
 #endif
