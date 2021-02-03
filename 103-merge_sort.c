42773c2e216551789ee2d3487bfe6cc4df658e5f
martinmsaavedra <martin@bildung.uy>
2021-02-03 15:37:38 +0000

    merge sort

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -16,25 +16,84 @@ void mergeSort(int *array, size_t left, size_t right)
     if (left < right)
     {
         mid = left + (right - left) / 2;
-        printf("MID %lu\n", mid);
         mergeSort(array, left, mid);
         mergeSort(array, mid + 1, right);
         merge(array, left, mid, right);
     }
 }
 
-void merge(int *array, size_t left, size_t mid, size_t right)
+void merge(int *arr, int p, int q, int r)
 {
-    size_t n1 = 0, n2 = 0;
 
-    (void)array;
-    printf("L = %lu\n", left);
-    printf("MID %lu\n", mid);
-    printf("R = %lu\n", right);
-    n1 = mid - (right + 1);
-    n2 = left - mid;
-    printf("Merging...\n");
-    printf("n1 %lu\n", n1);
-    printf("n2 %lu\n", n2);
-    return;
+    int n1 = q - p + 1;
+    int n2 = r - q;
+    int i, j, k, count = 0, index = 0;
+    int *L, *M;
+
+    printf("Mergin...\n");
+    L = malloc(sizeof(int) * n1);
+    M = malloc(sizeof(int) * n2);
+    printf("[left]: ");
+    for (i = 0; i < n1; i++)
+    {
+        L[i] = arr[p + i];
+        if (i + 1 == n1)
+            printf("%d\n", L[i]);
+        else
+            printf("%d, ", L[i]);
+    }
+    printf("[right]: ");
+    for (j = 0; j < n2; j++)
+    {
+        M[j] = arr[q + 1 + j];
+        if (j + 1 == n2)
+            printf("%d\n", M[j]);
+        else
+            printf("%d, ", M[j]);
+    }
+    i = 0;
+    j = 0;
+    k = p;
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= M[j])
+        {
+            arr[k] = L[i];
+            count++;
+            i++;
+        }
+        else
+        {
+            arr[k] = M[j];
+            count++;
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1)
+    {
+        arr[k] = L[i];
+        count++;
+        i++;
+        k++;
+    }
+
+    while (j < n2)
+    {
+        arr[k] = M[j];
+        count++;
+        j++;
+        k++;
+    }
+    free(L);
+    free(M);
+    printf("[Done]: ");
+    for (index = p; index < p + count; index++)
+    {
+        if (index + 1 == p + count)
+            printf("%d\n", arr[index]);
+        else
+            printf("%d, ", arr[index]);
+    }
 }
