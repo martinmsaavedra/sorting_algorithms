bf21a20c170d37cef0aaf888d70962ee1f2bc372
martinmsaavedra <martin@bildung.uy>
2021-02-03 15:00:48 +0000

    radix sort

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -1,78 +1,135 @@
-#include "sort.h" 
+#include "sort.h"
+
 /**
- * radix_sort -
+ * is_in_3 - count how many times appear an element in an array
+ * @copy_array: the array
+ * @size: the size of the array
+ * @num: element to find
+ * @divisor: divisor
+ * Return: count of times to appear
+ */
+int is_in_3(int *copy_array, size_t size, int num, int divisor)
+{
+	size_t i = 0;
+	int tmp = 0;
+
+	for (; i < size; i++)
+	{
+		if ((copy_array[i] / divisor) % 10 == num)
+		{
+			tmp = copy_array[i];
+			copy_array[i] = -1;
+			break;
+		}
+	}
+	return (tmp);
+}
+
+/**
+ * is_in_2 - count how many times appear an element in an array
+ * @array: the array
+ * @size: the size of the array
+ * @num: element to find
+ * @divisor: divisor
+ * Return: count of times to appear
+ */
+
+int is_in_2(int *array, size_t size, int num, int divisor)
+{
+	size_t i = 0;
+	int count = 0;
+
+	for (; i < size; i++)
+	{
+		if ((array[i] / divisor) % 10 == num)
+			count++;
+	}
+	return (count);
+}
+
+/**
+ * countingSort - sort an array using counting sort algorithm
+ * @array: The array
+ * @size: The size of the array
+ * @divisor: divisor
+ * Return: Nothing
+ */
+
+void countingSort(int *array, size_t size, int divisor)
+{
+	int *count_array = NULL, count = 0;
+	int i = 0, j = 0, index = 0;
+	int *copy_array = malloc(sizeof(int) * size);
+
+	for (i = 0; i < (int)size; i++)
+	{
+		copy_array[i] = array[i];
+	}
+	count_array = malloc(sizeof(int) * 10);
+	for (i = 0; i < 10; i++)
+	{
+		count += is_in_2(array, size, i, divisor);
+		count_array[i] = count;
+	}
+
+	for (i = 0; i < 10; i++)
+	{
+		if (count_array[i + 1] > count_array[i] && i + 1 <= 9)
+		{
+			j = count_array[i + 1] - count_array[i];
+			for (; j > 0; j--)
+			{
+				array[index++] = is_in_3(copy_array, size, i + 1, divisor);
+			}
+		}
+	}
+	free(count_array);
+	free(copy_array);
+}
+
+/**
+ * largest_int - return the largest int in array
+ * @array: the array
+ * @size: the size of the array
+ * Return: the largest int
+ */
+
+int largest_int(int *array, size_t size)
+{
+	size_t i = 0;
+	int largest = array[0];
+
+	for (; i < size; i++)
+	{
+		if (largest < array[i + 1] && i + 1 < size)
+			largest = array[i + 1];
+	}
+	return (largest);
+}
+
+/**
+ * radix_sort - sort array with radix_sort algorithm
  * @array: The array
  * @size: The size of the array
  * Return: Nothing
  */
-int LSD(int *array, size_t size)
-{
-    size_t i = 0;
-    int lsd = 0;
-    if (array != NULL)
-    {
-        lsd = array[0];
-        for (; i < size; i++)
-        {
-            if (lsd > array[i])
-                lsd = array[i];
-        }
-        return (lsd);
-    }
-    else
-        return (0);
-}
-
-int largest_int(int *array, size_t size)
-{
-    size_t i = 0;
-    int largest = array[0];
-    for (; i < size; i++)
-    {
-        if (largest < array[i + 1] && i + 1 < size)
-            largest = array[i + 1];
-    }
-    return (largest);
-}
-
-int is_in(int *array, size_t size, int num)
-{
-    size_t i = 0;
-    int count = 0;
-    for (; i < size; i++)
-    {
-        if (num == array[i])
-            count++;
-    }
-    return (count);
-}
 
 void radix_sort(int *array, size_t size)
 {
-    int *radix = NULL, lsd = 0, largest = 0, large_arr = 0;
-    int i = 0, j = 0, k = 0;
-    int count = 0;
-    lsd = LSD(array, size);
-    largest = largest_int(array, size);
-    large_arr = largest - lsd;
-    if (array != NULL && size > 1)
-    {
-        radix = malloc(sizeof(int) * large_arr + 1);
-        for (j = lsd; j < largest + 1; j++)
-        {
-            count = is_in(array, size, j);
-            radix[j - lsd] = count;
-        }
-        for (j = 0; j < large_arr + 1; j++)
-        {
-            if (radix[j] != 0)
-            {
-                for (k = radix[j]; k > 0; k--)
-                    array[i++] = j + lsd;
-                /*printf("%d ->  %d\n", radix[j], j + lsd);*/
-            }
-        }
-        free(radix);
-    }
-    print_array(array, size);
+	int largest = 0, digits = 0, i = 0, divisor = 1;
+
+	if (array == NULL || size < 2)
+		return;
+	largest = largest_int(array, size);
+	while (largest != 0)
+	{
+		digits++;
+		largest /= 10;
+	}
+	for (i = 0; i < digits; i++)
+	{
+		countingSort(array, size, divisor);
+		divisor *= 10;
+		print_array(array, size);
+	}
 }
